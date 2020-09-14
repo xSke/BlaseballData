3e9db692a5c0a8fc1c6ebd2cb9393017fb8d9727
Astrid <me@astr.cc>
2020-09-14 19:57:40 +0200

    whoops

---
+++ Blase.Server/Controllers/Models/SimResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Text.Json;
+
+namespace Blase.Server.Controllers.Models
+{
+    public class SimResponse
+    {
+        public DateTimeOffset Timestamp { get; set; }
+        public JsonElement Sim { get; set; }
+    }
+}
